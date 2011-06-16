4b0c728cc47d14445070430754b7f3850e654474
Jesse Wonsowski <jkw313@hotmail.com>
2011-06-15 19:43:34 -0500

    fixed test
    Signed-off-by: Jesse Wonsowski <jkw313@hotmail.com>

--- Artis/Artis.Tests/Controllers/HomeControllerTest.cs
+++ Artis/Artis.Tests/Controllers/HomeControllerTest.cs
@@ -22,7 +22,7 @@ namespace Artis.Tests.Controllers
             ViewResult result = controller.Index() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Welcome to ASP.NET MVC!", result.ViewBag.Message);
+            Assert.AreEqual("Welcome to Artis!", result.ViewBag.Message);
         }
 
         [TestMethod]
